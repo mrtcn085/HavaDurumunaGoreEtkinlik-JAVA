5d6be0f4f0432e36c011e2d3556ba0752c6e409d
mrtcn085 <101869750+mrtcn085@users.noreply.github.com>
2022-09-20 09:29:03 +0300

    Add files via upload

---
+++ HavaSicakligiEtkinlikOner/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklık Giriniz : ");
+        heat = input.nextInt();
+        if(heat < 5) {
+            System.out.println("Kayak Yapabilirsiniz.");
+        } else if (heat >= 5 && heat <=25 ) {
+            if (heat <= 15) {
+                System.out.println("Sinemaya Gidebilirsiniz.");
+            }
+            if (heat >= 10) {
+                System.out.println("Pikniğe Gidebilirsiniz.");
+            }
+        } else {
+            System.out.println("Yüzmeye Gidebilirsin.");
+        }
+
+    }
+}
